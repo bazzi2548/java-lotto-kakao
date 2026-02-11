5f383d6285d77ee4f12078444c82d957d0c391cd
문정환 <bazzi2548@naver.com>
2026-02-11 11:18:42 +0900

    refactor: private 생성자 추가 - 객체 생성이 필요 없는 클래스이므로 private

--- src/main/java/lotto/LottoJudge.java
+++ src/main/java/lotto/LottoJudge.java
@@ -1,6 +1,10 @@
 package lotto;
 
 public class LottoJudge {
+    private LottoJudge() {
+
+    }
+
     public static Rank judge(WinningLotto winning, Lotto lotto){
         int count = winning.countMatchingNumbers(lotto);
         boolean matchBonus = winning.matchBonus(lotto);
