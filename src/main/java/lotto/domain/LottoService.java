5369542da993423edc9fdacb5f1e00a4edb7e0e3
문정환 <bazzi2548@naver.com>
2026-02-12 15:58:25 +0900

    feat: 수동 구매 구현

--- src/main/java/lotto/domain/LottoService.java
+++ src/main/java/lotto/domain/LottoService.java
@@ -4,11 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LottoService {
-    public LottoBundle purchase(int count) {
-        List<Lotto> purchasedLottos = new ArrayList<>();
-        for (int i = 0; i < count; i++) {
-            purchasedLottos.add(LottoGenerator.generateLotto());
-        }
-        return new LottoBundle(purchasedLottos);
-    }
+
+	public LottoBundle purchaseAuto(int count) {
+		List<Lotto> purchasedLottos = new ArrayList<>();
+		for (int i = 0; i < count; i++) {
+			purchasedLottos.add(LottoGenerator.generateLotto());
+		}
+		return new LottoBundle(purchasedLottos);
+	}
 }
