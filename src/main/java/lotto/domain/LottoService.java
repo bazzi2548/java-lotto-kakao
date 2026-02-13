b0f7ffb896292e98c55443a5e37cff95ada426a1
문정환 <70272622+bazzi2548@users.noreply.github.com>
2026-02-13 20:04:28 +0900

    refacotr: 불변 리스트로 변환
    Co-authored-by: 최진영 <jinyoungchoi95@gmail.com>

--- src/main/java/lotto/domain/LottoService.java
+++ src/main/java/lotto/domain/LottoService.java
@@ -6,10 +6,9 @@ import java.util.List;
 public class LottoService {
 
 	public LottoBundle purchaseAuto(int count) {
-		List<Lotto> purchasedLottos = new ArrayList<>();
-		for (int i = 0; i < count; i++) {
-			purchasedLottos.add(LottoGenerator.generateLotto());
-		}
+		List<Lotto> purchasedLottos = IntStream.range(0, count)
+				.mapToObj(it -> LottoGenerator.generateLotto())
+				.toList();
 		return new LottoBundle(purchasedLottos);
 	}
 }
